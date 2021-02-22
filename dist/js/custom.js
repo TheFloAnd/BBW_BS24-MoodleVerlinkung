9e7f35805455b0ba9c1c67a7bb6380856cea56ac
TheFloAnd <florian.familieholst@gmail.com>
2021-02-22 15:21:47 +0100

    Structure

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -39,16 +39,16 @@ function get_img() {
 */
 function random_background() {
     var img = [
-        "url('/src/img/background/bg-1.jpg')",
-        "url('/src/img/background/bg-2.jpg')",
-        "url('/src/img/background/bg-3.jpg')",
-        "url('/src/img/background/bg-4.jpg')",
-        "url('/src/img/background/bg-5.jpg')",
-        "url('/src/img/background/bg-6.jpg')",
-        "url('/src/img/background/bg-7.jpg')",
-        "url('/src/img/background/bg-8.jpg')",
-        "url('/src/img/background/bg-9.jpg')",
-        "url('/src/img/background/bg-10.jpg')",
+        "url('/dist/img/background/bg-1.jpg')",
+        "url('/dist/img/background/bg-2.jpg')",
+        "url('/dist/img/background/bg-3.jpg')",
+        "url('/dist/img/background/bg-4.jpg')",
+        "url('/dist/img/background/bg-5.jpg')",
+        "url('/dist/img/background/bg-6.jpg')",
+        "url('/dist/img/background/bg-7.jpg')",
+        "url('/dist/img/background/bg-8.jpg')",
+        "url('/dist/img/background/bg-9.jpg')",
+        "url('/dist/img/background/bg-10.jpg')",
     ];
 
     var random = Math.floor(Math.random() * img.length);
@@ -56,13 +56,13 @@ function random_background() {
 }
 
 function random_preview() {
-    var bbw = ["/src/img/preview/bbw/bbw.jpeg"];
+    var bbw = ["/dist/img/preview/bbw/bbw.jpeg"];
     var bs24 = [
-        "/src/img/preview/bs24/bs24-1.jpeg",
-        "/src/img/preview/bs24/bs24-2.jpeg",
-        "/src/img/preview/bs24/bs24-3.jpeg",
-        "/src/img/preview/bs24/bs24-4.jpeg",
-        "/src/img/preview/bs24/bs24-5.jpeg",
+        "/dist/img/preview/bs24/bs24-1.jpeg",
+        "/dist/img/preview/bs24/bs24-2.jpeg",
+        "/dist/img/preview/bs24/bs24-3.jpeg",
+        "/dist/img/preview/bs24/bs24-4.jpeg",
+        "/dist/img/preview/bs24/bs24-5.jpeg",
     ];
     var img_bbw = document.getElementById("preview-img-bbw");
     var img_bs24 = document.getElementById("preview-img-bs24");
